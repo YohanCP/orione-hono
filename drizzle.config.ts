98671a23e9a8567bc34ca15fb91895fcc0331ef6
Nero <145282836+YohanCP@users.noreply.github.com>
2025-11-15 16:26:18 +0700

    v0.1

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  out: './drizzle',
+  out: './.drizzle',
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
