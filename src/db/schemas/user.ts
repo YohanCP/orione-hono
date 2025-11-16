991a60b76ce20e6bb5e4d53b914528fa74b6e707
Nero <145282836+YohanCP@users.noreply.github.com>
2025-11-16 22:52:36 +0700

    Teset 16

--- src/db/schemas/user.ts
+++ src/db/schemas/user.ts
@@ -1,7 +1,7 @@
 import { sql } from "drizzle-orm";
 import { pgTable, serial, text, timestamp, boolean, uniqueIndex, pgEnum } from "drizzle-orm/pg-core";
 
-export const accountType = pgEnum('user_role',['user', 'seller']);
+export const userRoleEnum = pgEnum('user_role', ['user', 'seller']);
 
 export const users = pgTable(
   "users",
@@ -11,7 +11,7 @@ export const users = pgTable(
     email: text("email").notNull(),
     password: text("password").notNull(),
     createdAt: timestamp("createdAt").defaultNow().notNull(),
-    role: accountType('role').notNull().default('user'),
+    role: userRoleEnum('role').notNull().default('user'),
     isValidated: boolean("is_validated").default(false).notNull(),
   },
   (table) => ({
